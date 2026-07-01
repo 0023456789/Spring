b782253954ff56c01210953e3a19fcec8fdff2de
PHAM THANH HUNG <phamthanhheroc6@gmail.com>
2026-07-01 09:56:26 +0700

    Video 6: encoding with bycrypt

---
+++ src/main/java/com/example/Second/Controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.example.Second.Controller;
+
+
+import com.example.Second.Service.AuthenticationService;
+import com.example.Second.dto.request.ApiResponse;
+import com.example.Second.dto.request.AuthenticationRequest;
+import com.example.Second.dto.response.AuthenticationResponse;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationController {
+    AuthenticationService authenticationService;
+
+    @PostMapping("/log-in")
+    ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        boolean result = authenticationService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>builder()
+                .result(AuthenticationResponse.builder()
+                        .authenticated(result)
+                        .build())
+                .build();
+    }
+}
