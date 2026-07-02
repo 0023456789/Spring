517995ae1218ca5c13f47afaf7e83bce9802a8c4
PHAM THANH HUNG <phamthanhheroc6@gmail.com>
2026-07-02 14:27:17 +0700

    video 8 config JWT

--- src/main/java/com/example/Second/Controller/UserController.java
+++ src/main/java/com/example/Second/Controller/UserController.java
@@ -8,6 +8,7 @@ import com.example.Second.dto.response.UserResponse;
 import jakarta.validation.Valid;import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.List;
 @RequestMapping("/users")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class UserController {
     UserService userService;
 
