d1ea8a24bc88960431ec36eb8d2845a4b5471c7f
PHAM THANH HUNG <phamthanhheroc6@gmail.com>
2026-07-03 13:42:26 +0700

    Video 9:Delegate authorization using jwt

--- src/main/java/com/example/Second/Controller/UserController.java
+++ src/main/java/com/example/Second/Controller/UserController.java
@@ -8,6 +8,11 @@ import com.example.Second.dto.response.UserResponse;
 import jakarta.validation.Valid;import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+<<<<<<< Updated upstream
+=======
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+>>>>>>> Stashed changes
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,6 +33,11 @@ public class UserController {
 
     @GetMapping
     ApiResponse<List<UserResponse>> getUsers(){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        log.info("Username: {}", authentication.getName());
+        authentication.getAuthorities().forEach(grantedAuthority -> log.info(grantedAuthority.getAuthority()));
+
         return ApiResponse.<List<UserResponse>>builder()
                 .result(userService.getUsers())
                 .build();
--- src/main/java/com/example/Second/Service/UserService.java
+++ src/main/java/com/example/Second/Service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.Second.Service;
 
+import com.example.Second.Enum.Role;
 import com.example.Second.Exception.AppException;
 import com.example.Second.Exception.ErrorCode;
 import com.example.Second.Repository.UserRepository;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -24,14 +26,20 @@ import java.util.List;
 public class UserService {
     UserRepository userRepository;
     UserMapper userMapper;
+    PasswordEncoder passwordEncoder;
 
     public UserResponse createUser(UserCreationRequest request){
         if (userRepository.existsByUsername(request.getUsername()))
             throw new AppException(ErrorCode.USER_EXISTED);
 
         User user = userMapper.toUser(request);
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
         user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        HashSet<String> roles = new HashSet<>();
+        roles.add(Role.USER.name());
+
+        user.setRoles(roles);
+
         return userMapper.toUserResponse(userRepository.save(user));
     }
 
